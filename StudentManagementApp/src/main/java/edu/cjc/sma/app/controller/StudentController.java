b4efce19138fbd1e3c1a3fe2d5515d006870893c
pranit <borkarpranit@gmail.com>
2026-02-02 13:03:47 +0530

    Second Commit Login Page

---
+++ StudentManagementApp/src/main/java/edu/cjc/sma/app/controller/StudentController.java
@@ -0,0 +1,14 @@
+package edu.cjc.sma.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StudentController {
+
+	@RequestMapping("/")
+	public String preLogin()
+	{
+		return "login";
+	}
+}
